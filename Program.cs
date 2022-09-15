5820a9cd53642283accac3118cfba0f1bc45cb71
KarinaIvanovax <112822612+KarinaIvanovax@users.noreply.github.com>
2022-09-15 12:05:18 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace home10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //мир труд май
+            Console.WriteLine("Мир Труд Май");
+            var a = ("Мир");
+            var b = ("Труд");
+            var c = ("Май");
+            Console.WriteLine($"{a}\n   {b}\n         {c}\n");
+        }
+    }
+}
