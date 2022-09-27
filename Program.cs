55b15a671b12071947d3c4c6423d255fd951d9f6
KarinaIvanovax <112822612+KarinaIvanovax@users.noreply.github.com>
2022-09-27 11:30:00 +0300

    Задача с возрастом

--- Program.cs
+++ Program.cs
@@ -4,103 +4,26 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassWork_ot_17._09._22
+namespace dolg
 {
-    //enum bankaccount
-    //{
-    //   current,
-    //   savings
-    //}
-    //struct bankaccount
-    //{
-    //    public int number;
-    //    public string type;
-    //    public int balance;
-    //}
-    //enum vuz
-    //{
-    //    kgu,
-    //    kai,
-    //    khti
-    //}
-    //struct person
-    //{
-    //    public string name;
-    //    public string vuz;
-    //}
     internal class Program
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Упражнение 3.1");
-            ////Создать перечислимый тип данных отображающий
-            ////виды банковского счета(текущий и сберегательный).Создать переменную типа
-            ////перечисления, присвоить ей значение и вывести это значение на печать.
-            //Console.WriteLine("Текущий счет => нажмите 1\nСберегательный счет => нажмите 2 ");
-            //int c = int.Parse(Console.ReadLine());
-            //bankaccount a = bankaccount.current;
-            //bankaccount b = bankaccount.savings;
-            ////Console.WriteLine("Средства на текущем счете: ");
-            ////double x = double.Parse(Console.ReadLine());
-            ////Console.WriteLine("Средства на сберегательном счете: ");
-            ////double y = double.Parse(Console.ReadLine());
-            //if (c == 1)
-            //{
-            //    Console.WriteLine($"Счет {bankaccount.current}");
-            //}
-            //else if (c == 2)
-            //{
-            //    Console.WriteLine($"Счет {bankaccount.savings}");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Error");
-            //}
-            //Console.ReadKey();
-
-            //Console.WriteLine("Упражнение 3.2");
-            ////Создать структуру данных, которая хранит информацию о банковском счете – его номер, тип и баланс.
-            ////Создать переменную такого типа, заполнить структуру значениями и напечатать результат.
-            //var account1 = new bankaccount();
-            //Console.Write("Номер счета 1: ");
-            //account1.number = int.Parse(Console.ReadLine());
-            //account1.type = "Сберегательный";
-            //Console.Write("Баланс счета 1: ");
-            //account1.balance = int.Parse(Console.ReadLine());
-            //Console.WriteLine($"Счет 1\nНомер счета: {account1.number}\nТип: {account1.type}\nБаланс: {account1.balance}");
-            //var account2 = new bankaccount();
-            //Console.Write("Номер счета 2: ");
-            //account2.number = int.Parse(Console.ReadLine());
-            //account2.type = "Текущий";
-            //Console.Write("Баланс счета 2: ");
-            //account2.balance = int.Parse(Console.ReadLine());
-            //Console.WriteLine($"Счет 2\nНомер счета: {account2.number}\nТип: {account2.type}\nБаланс: {account2.balance}");
-            //Console.ReadKey();
-
-            //Console.WriteLine("Упражнение 3.1. ДомЗ");
-            ////Создать перечислимый тип ВУЗ {КГУ, КАИ,КХТИ}.
-            ////Создать структуру работник с двумя полями: имя, ВУЗ.Заполнить структуру данными и распечатать.
-            //Console.Write("Введите имя: ");
-            //string name = Console.ReadLine();
-            //Console.WriteLine("КГУ => нажмите 1\nКАИ => нажмите 2\nКХТИ => нажмите 3 ");
-            //int c = int.Parse(Console.ReadLine());
-            //if (c == 1)
-            //{
-            //    Console.WriteLine($"{name}, учится в КГУ");
-            //}
-            //else if (c == 2)
-            //{
-            //    Console.WriteLine($"{name}, учится в КАИ");
-            //}
-            //else if (c == 3)
-            //{
-            //    Console.WriteLine($"{name}, учится в КХТИ");
-            //}
-            //else
-            //{
-            //    Console.WriteLine("Ошибка");
-            //}
-            //Console.ReadKey();
+            Console.Write("Ваша дата рождения: ");
+            DateTime birthday = Convert.ToDateTime(Console.ReadLine());
+            DateTime today = DateTime.Today;
+            if (today.Month >= birthday.Month && today.Day >= birthday.Day)
+            {
+                int age = today.Year - birthday.Year;
+                Console.WriteLine("Ваш возраст: " + age);
+            }
+            else
+            {
+                int age = today.Year - birthday.Year - 1;
+                Console.WriteLine("Ваш возраст: " + age);
+            }
+            Console.ReadKey();
         }
     }
 }
