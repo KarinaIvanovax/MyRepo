626b54763d0443c2a829e5061b180c520ceb5f87
KarinaIvanovax <112822612+KarinaIvanovax@users.noreply.github.com>
2022-11-18 17:10:19 +0300

    ДЗ

--- Class1.cs
+++ Class1.cs
@@ -4,104 +4,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_9
+namespace DZ
 {
-    enum BankAcc
+    enum Stage
     {
-        savings,
-        current
+        Проект, Исполнение, Закрыт
     }
-    enum Songs
+    internal class Project
     {
-        song11,
-        song22,
-        song33,
-        song44,
-    }
-    internal class Savings
-    {
-        public int accnumber;
-        public double balanceS;
-        public BankAcc type;
-        public Savings()
+        public string DescriptionP { get; set; }
+        public DateTime TermP { get; set; }
+        public string CustomerP { get; set; }
+        public string TeamleadP { get; set; }
+        //public Task Tasks { get; set; }
+        public Stage StatusP { get; set; }
+        public override string ToString()
         {
-            Random random = new Random();
-            int a = random.Next(1000, 9999);
-            accnumber = a;
-            balanceS = 80000;
-            type = BankAcc.savings;
+            return $"Описание проекта: {DescriptionP}\nЗаказчик: {CustomerP}";
         }
-        public void PrintSavings()
+        class Task
         {
-            Console.WriteLine($"Номер счета: {accnumber}\nБаланс: {balanceS}\nТип счета: {type}");
-        }
-        public void MoneyTransferS(int amount)
-        {
-            this.balanceS -= amount;
-            Current balanceC += amount;
-        }
-    }
-    internal class Current
-    {
-        public int accnumber;
-        public double balanceC;
-        public BankAcc type;
-        public Current()
-        {
-            Random random = new Random();
-            int b = random.Next(1000, 9999);
-            accnumber = b;
-            balanceC = 80000;
-            type = BankAcc.current;
-        }
-        public void PrintCurrent()
-        {
-            Console.WriteLine($"Номер счета: {accnumber}\nБаланс: {balanceC}\nТип счета: {type}");
-        }
-        public void MoneyTransferC(int amount)
-        {
-            this.balanceC -= amount;
-            Savings balanceS += amount;
-        }
+            public string DescriptionT { get; set; }
+            public DateTime TermT { get; set; }
+            public string CustomerT { get; set; }
+            public string TeamleadT { get; set; }
+            public Stage StatusT { get; set; }
+            public Report ReportT { get; set; }
 
-    }
-
-
-    internal class Song
-    {
-        public string Name { get; set; }
-        public string Author { get; set; }
-        public Songs prev { get; set; }
-        public override string ToString() { return $"Название: {Name}\nАвтор: {Author}\n:{prev}"; }
-        public void Title()
-        {
-            Song song1 = new Song { Name = "1", Author = "11", prev = Songs.song11 };
-            Console.WriteLine(song1.ToString());
-            Song song2 = new Song { Name = "2", Author = "22", prev = Songs.song22-1};
-            Console.WriteLine(song2.ToString());
-            Song song3 = new Song { Name = "3", Author = "33", prev = Songs.song33-1};
-            Console.WriteLine(song3.ToString());
-            Song song4 = new Song { Name = "4", Author = "44", prev = Songs.song44-1};
-            Console.WriteLine(song4.ToString());
         }
-        public override bool Equals(object d)
+        class Report
         {
-            Song song = d as Song;
-            if (song != null)
-            {
-                if (Name == song.Name && Author == song.Author)
-                {
-                    return true;
-                }
-                else
-                {
-                    return false;
-                }
-            }
-            return false;
-        }
 
-        public override int GetHashCode() => Name.GetHashCode();
+        }
     }
+   
 
 }
--- Program.cs
+++ Program.cs
@@ -1,54 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Lesson_9
+namespace DZ
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Задача 9.1");
-            Savings sav = new Savings();
-            sav.PrintSavings();
-            Current cur = new Current();
-            cur.PrintCurrent();
-            Console.WriteLine();
-            Console.WriteLine("\nХотите перевести деньги из первого банка во второй?");
-            string answer1 = Console.ReadLine();
-            if (answer1 == "да" || answer1 == "Да")
-            {
-                Console.WriteLine("С какого счета хотите перевести?");
-                string answer2 = Console.ReadLine();
-                if (answer2 == "сберегательный")
-                {
-                    Console.WriteLine("Какую сумму хотите перевести?");
-                    int amount = Convert.ToInt32(Console.ReadLine());
-                    sav.MoneyTransferS(amount);
-                }
-                else if (answer2 == "текущий")
-                {
-                    Console.WriteLine("Какую сумму хотите перевести?");
-                    int amount = Convert.ToInt32(Console.ReadLine());
-                    cur.MoneyTransferC(amount);
-                }
-                else
-                {
-                    Console.WriteLine("Завершение программы");
-                }
-            }
-            else
-            {
-                Console.WriteLine("Завершение программы");
-            }
-            Console.WriteLine();
-
-            Console.WriteLine("Домашнее задание 9.1");
-            Song mySong = new Song();
-            mySong.Title();
         }
     }
 }
